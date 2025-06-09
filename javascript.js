1b7bd294b2599d15cd138d24882832d00a127ed9
rmergeee <63661366+rmergeee@users.noreply.github.com>
2025-06-09 19:44:31 +0300

    create js file and rename html file

---
+++ javascript.js
@@ -0,0 +1,27 @@
+console.log(23 + 97);
+
+console.log(34 + 12 + 41 + 11 + 22 + 2);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+
+console.log(a);
+
+a = 1;
+
+console.log(a);
+
+let b = 7 * a;
+
+console.log(b);
+
+const max = 57;
+
+const actual = max - 13;
+
+const percentage = actual / max;
+
+console.log(percentage);
+
+alert("Ваше повідомлення тут!");
