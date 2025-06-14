aedc0677a1c18a6948a9301d1f1b3a6e0ec8d3be
rmergeee <63661366+rmergeee@users.noreply.github.com>
2025-06-15 01:26:50 +0300

    fizzbizz

--- javascript.js
+++ javascript.js
@@ -1,29 +1,15 @@
-function add7(num) {
-  return num + 7;
-}
+// FizzBuzz
 
-function add8(num) {
-  if (typeof(num) == typeof(4)) {
-    return num + 8;
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+
+for (let i=1;i<=answer;i++) {
+  if ( i % 5 === 0 && i % 3 === 0 ) {
+    console.log("FizzBuzz");
+  } else if ( i % 5 === 0 ) {
+    console.log("Buzz");
+  } else if ( i % 3 === 0 ) {
+    console.log("Fizz");
+  } else {
+    console.log(i);
   }
 }
-
-let multiply = function(num1, num2) {
-  return num1 * num2;
-}
-
-let capitalize = str => str.toUpperCase().substr(0,1);
-
-let lastLetter = str => str.slice(-1);
-
-console.log(add7(4));
-
-console.log(add8(4));
-
-console.log(typeof(4))
-
-console.log(multiply(2,2));
-
-console.log(capitalize("String"));
-
-console.log(lastLetter("String"));
