847c8265fc7b347a27c8270271cffedd18d2fd82
rmergeee <63661366+rmergeee@users.noreply.github.com>
2025-07-09 20:33:42 +0300

    clean rep

--- javascript.js
+++
@@ -1,9 +0,0 @@
-const cats = ["Pete", "Biggles", "Jasmine"];
-
-let myFavoriteCats = "My cats are called ";
-
-for (const cat of cats) {
-  myFavoriteCats += `${cat}, `;
-}
-
-console.log(myFavoriteCats);
